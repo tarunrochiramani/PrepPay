8f54bbdef78c234e25ec64f1a4fd3bccb21751fd
Tarun Rochiramani <tarunrochiramani@gmail.com>
2017-07-08 16:59:03 -0700

    Binary Search Tree methods

--- src/main/java/com/tr/ds/services/BSTService.java
+++ src/main/java/com/tr/ds/services/BSTService.java
@@ -1,5 +1,8 @@
 package com.tr.ds.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.tr.ds.BSTNode;
 import com.tr.exceptions.InvalidInputException;
 
@@ -44,6 +47,23 @@ public class BSTService {
         }
     }
 
+    public int maxDepth(BSTNode treenode) {
+        if (treenode == null) {
+            return 0;
+        }
+
+        if (treenode.getLeft() == null && treenode.getRight() == null) {
+            return 1;
+        }
+
+        int leftDepth = maxDepth(treenode.getLeft());
+        int rightDepth = maxDepth(treenode.getRight());
+
+        int currentMax = leftDepth >= rightDepth ? leftDepth : rightDepth;
+
+        return currentMax+1;
+    }
+
     public void printInorder(BSTNode head) {
         if (head == null) {
             return;
@@ -51,5 +71,115 @@ public class BSTService {
         printInorder(head.getLeft());
         System.out.println(head.getValue());
         printInorder(head.getRight());
+    }
+
+    public boolean hasPathSum(BSTNode treenode, int sum) {
+        if (treenode == null) {
+            return false;
+        }
+
+        if (treenode.getValue() > sum) {
+            return false;
+        }
+
+        if (treenode.getValue() == sum && treenode.getLeft() == null && treenode.getRight() == null) {
+            return true;
+        }
+
+        int remainingSum = sum - treenode.getValue();
+        boolean leftHasPathSum = hasPathSum(treenode.getLeft(), remainingSum);
+        if (leftHasPathSum) {
+            return true;
+        }
+
+        return hasPathSum(treenode.getRight(), remainingSum);
+    }
+
+    public String printPaths(BSTNode treeNode) throws InvalidInputException {
+        List<BSTNode> nodesVisited = new ArrayList<BSTNode>();
+        StringBuilder stringBuilder = new StringBuilder();
+        printPaths(treeNode, nodesVisited, stringBuilder);
+        return stringBuilder.toString();
+    }
+
+    public void mirror(BSTNode treeNode) throws InvalidInputException {
+        if (treeNode == null) {
+            throw new InvalidInputException();
+        }
+
+        if (treeNode.getLeft() != null) {
+            mirror(treeNode.getLeft());
+        }
+
+        if (treeNode.getRight() != null) {
+            mirror(treeNode.getRight());
+        }
+
+        BSTNode temp = treeNode.getLeft();
+        treeNode.setLeft(treeNode.getRight());
+        treeNode.setRight(temp);
+    }
+
+    public void doubleTree(BSTNode treeNode) {
+        if (treeNode == null) {
+            return;
+        }
+
+        doubleTree(treeNode.getLeft());
+
+        BSTNode newnode = new BSTNode();
+        newnode.setValue(treeNode.getValue());
+        newnode.setLeft(treeNode.getLeft());
+        treeNode.setLeft(newnode);
+
+        doubleTree(treeNode.getRight());
+    }
+
+    private void printPaths(BSTNode treeNode, List<BSTNode> nodesVisited, StringBuilder stringBuilder) throws InvalidInputException {
+        if (treeNode == null) {
+            throw new InvalidInputException();
+        }
+
+        if (treeNode.getLeft() == null && treeNode.getRight() == null) {
+            for (BSTNode node : nodesVisited) {
+                stringBuilder.append(node.getValue());
+                stringBuilder.append(" -> ");
+            }
+            stringBuilder.append(treeNode.getValue());
+            stringBuilder.append("\n");
+            return;
+        }
+
+        nodesVisited.add(treeNode);
+        if (treeNode.getLeft() != null) {
+            printPaths(treeNode.getLeft(), nodesVisited, stringBuilder);
+        }
+
+        if (treeNode.getRight() != null) {
+            printPaths(treeNode.getRight(), nodesVisited, stringBuilder);
+        }
+        nodesVisited.remove(treeNode);
+    }
+
+    public boolean sameTree(BSTNode treenodeA, BSTNode treeNodeB) {
+        if (treenodeA == null && treeNodeB == null) {
+            return true;
+        }
+
+        if (treenodeA == null || treeNodeB == null) {
+            return false;
+        }
+
+        if (treenodeA.getValue() != treeNodeB.getValue()) {
+            return false;
+        }
+
+        boolean leftsAreSame = sameTree(treenodeA.getLeft(), treeNodeB.getLeft());
+        if (!leftsAreSame) {
+            return false;
+        }
+        boolean rightsAreSame = sameTree(treenodeA.getRight(), treeNodeB.getRight());
+
+        return leftsAreSame & rightsAreSame;
     }
 }
--- src/test/java/com/tr/ds/services/BSTServiceTest.java
+++ src/test/java/com/tr/ds/services/BSTServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -23,6 +24,107 @@ public class BSTServiceTest {
         assertNotNull(root);
         assertEquals(values.length, assertIsBST(root));
     }
+
+    @Test
+    public void cantGetMaxDepthForBalancedTree() throws InvalidInputException {
+        int[] values = new int[] {7, 3, 9, 2, 4, 8, 10, 5 ,1};
+        BSTNode root = service.createTree(values);
+
+        int maxDepth = service.maxDepth(root);
+
+        assertTrue(maxDepth > 0);
+        assertEquals(4 , maxDepth);
+    }
+
+    @Test
+    public void cantGetMaxDepthForUnBalancedTree() throws InvalidInputException {
+        int[] values = new int[] {1, 2, 3, 4, 5};
+        BSTNode root = service.createTree(values);
+
+        int maxDepth = service.maxDepth(root);
+
+        assertTrue(maxDepth > 0);
+        assertEquals(5 , maxDepth);
+    }
+
+    @Test
+    public void doesHaveSumForOneNode() throws InvalidInputException {
+        int[] values = new int[] {5};
+        BSTNode root = service.createTree(values);
+
+        assertTrue(service.hasPathSum(root, 5));
+        assertFalse(service.hasPathSum(root, 10));
+    }
+
+    @Test
+    public void doesHaveSum() throws InvalidInputException {
+        int[] values = new int[] {10, 20, 8, 9, 4, 4};
+        BSTNode root = service.createTree(values);
+
+        assertTrue(service.hasPathSum(root, 27));
+    }
+
+    @Test
+    public void canPrintPaths() throws InvalidInputException {
+        int[] values = new int[] {7, 3, 9, 2, 4, 8, 10, 5 ,1};
+        BSTNode root = service.createTree(values);
+
+        String paths = service.printPaths(root);
+
+        assertNotNull(paths);
+        System.out.println(paths);
+    }
+
+    @Test
+    public void canMirror() throws InvalidInputException {
+        int[] values = new int[] {7, 3, 9, 2, 4, 8, 10, 5 ,1};
+        BSTNode root = service.createTree(values);
+
+        service.mirror(root);
+
+        int countOfNodes = assertMirrorBST(root);
+        assertEquals(values.length, countOfNodes);
+    }
+
+    @Test
+    public void canDoubleTree() throws InvalidInputException {
+        int[] values = new int[] {7, 3, 9, 2, 4, 8, 10, 5 ,1};
+        BSTNode root = service.createTree(values);
+
+        service.doubleTree(root);
+        service.printInorder(root);
+
+        int countOfNode = assertIsBST(root);
+        assertEquals(values.length * 2, countOfNode);
+    }
+
+    @Test
+    public void isSameTree() throws InvalidInputException {
+        int[] values = new int[] {7, 3, 9, 2, 4, 8, 10, 5 ,1};
+        BSTNode root = service.createTree(values);
+
+        assertTrue(service.sameTree(root, root));
+    }
+
+    @Test
+    public void isSameTreeWithNoNodes() throws InvalidInputException {
+        BSTNode root = null;
+
+        assertTrue(service.sameTree(root, root));
+    }
+
+    @Test
+    public void areNotSameTree() throws InvalidInputException {
+        int[] values = new int[] {7, 3, 9, 2, 4, 8, 10, 5 ,1};
+        BSTNode root = service.createTree(values);
+
+        int[] values1 = new int[] {3, 9, 2, 4, 8, 10, 5 ,1};
+        BSTNode root1 = service.createTree(values1);
+
+        assertFalse(service.sameTree(root, root1));
+    }
+
+
 
     private int assertIsBST(BSTNode root) {
         int countOfNodes = 0;
@@ -44,6 +146,31 @@ public class BSTServiceTest {
 
             assertTrue(root.getRight().getValue() > root.getValue());
             countOfNodes += assertIsBST(root.getRight());
+        }
+
+        return ++countOfNodes;
+    }
+
+    private int assertMirrorBST(BSTNode root) {
+        int countOfNodes = 0;
+        if (root == null) {
+            return countOfNodes;
+        }
+
+        if (root.getLeft() == null && root.getRight() == null) {
+            return ++countOfNodes;
+        }
+
+        if (root.getLeft() != null) {
+
+            assertTrue(root.getLeft().getValue() > root.getValue());
+            countOfNodes += assertMirrorBST(root.getLeft());
+        }
+
+        if (root.getRight() != null) {
+
+            assertTrue(root.getRight().getValue() <= root.getValue());
+            countOfNodes += assertMirrorBST(root.getRight());
         }
 
         return ++countOfNodes;
