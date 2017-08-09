190298a0ee70647dbdbdbe865afc21820650e6d3
Tarun Rochiramani <tarunrochiramani@gmail.com>
2017-08-09 07:31:29 -0700

    Knuth Morris Prat Substring implementation

--- src/main/java/com/tr/stringops/SubString.java
+++ src/main/java/com/tr/stringops/SubString.java
@@ -1,5 +1,7 @@
 package com.tr.stringops;
 
+import com.tr.exceptions.InvalidInputException;
+
 public class SubString {
 
     public boolean isSubstringBruteForce(String s1, String s2) {
@@ -23,5 +25,54 @@ public class SubString {
             }
         }
         return false;
+    }
+
+    public int[] kmpPrefix(String input) throws InvalidInputException {
+        if (input == null) {
+            throw new InvalidInputException();
+        }
+
+        int[] prefixValues = new int[input.length()];
+        int suffixIndex = 0;
+        prefixValues[0] = suffixIndex;
+        for (int pos = 1; pos < input.length(); pos++) {
+            while (suffixIndex > 0 && (input.charAt(suffixIndex) != input.charAt(pos))) {
+                suffixIndex = prefixValues[suffixIndex-1];
+            }
+            if (input.charAt(suffixIndex) == input.charAt(pos)) {
+                suffixIndex ++;
+            }
+            prefixValues[pos] = suffixIndex;
+        }
+        return prefixValues;
+    }
+
+    public int kmpSubstringSearch(String text, String pattern) throws InvalidInputException {
+        if (pattern == null || text == null) {
+            throw new InvalidInputException();
+        }
+
+        if (pattern.length() > text.length()) {
+            throw new InvalidInputException();
+        }
+
+
+        int[] prefixValues = kmpPrefix(pattern);
+        int patternPos = 0;
+        for (int textPos=0; textPos<text.length(); textPos++) {
+            while(patternPos > 0 && text.charAt(textPos) != pattern.charAt(patternPos)) {
+                patternPos = prefixValues[patternPos - 1];
+            }
+
+            if (text.charAt(textPos) == pattern.charAt(patternPos)) {
+                patternPos++;
+            }
+
+            if (patternPos == pattern.length()) {
+                return (textPos - pattern.length() + 1);
+            }
+        }
+
+        return -1;
     }
 }
--- src/test/java/com/tr/stringops/SubStringTest.java
+++ src/test/java/com/tr/stringops/SubStringTest.java
@@ -1,10 +1,14 @@
 package com.tr.stringops;
 
+import com.tr.exceptions.InvalidInputException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnit4.class)
@@ -18,5 +22,56 @@ public class SubStringTest {
         assertFalse(subString.isSubstringBruteForce(null, "abc"));
         assertFalse(subString.isSubstringBruteForce("ab", "abc"));
     }
+
+    @Test
+    public void canGetKMPPrefix() throws InvalidInputException {
+        int[] expectedPrefix = new int[] {0, 0, 1, 2, 3, 4, 0, 1};
+        int[] kmpPrefix = subString.kmpPrefix("abababca");
+
+        assertNotNull(kmpPrefix);
+        assertArrayEquals(expectedPrefix, kmpPrefix);
+    }
+
+    @Test
+    public void canGetKMPPrefix_1() throws InvalidInputException {
+        int[] expectedPrefix = new int[] {0, 0, 0, 0, 0, 1};
+        int[] kmpPrefix = subString.kmpPrefix("abbbba");
+
+        assertNotNull(kmpPrefix);
+        assertArrayEquals(expectedPrefix, kmpPrefix);
+    }
+
+
+    @Test
+    public void canDoKMPSubStringSearch() throws InvalidInputException {
+        int foundIndex = subString.kmpSubstringSearch("abcdefgh", "def");
+
+        assertEquals(3, foundIndex);
+    }
+
+    @Test
+    public void canDoKMPSubStringSearch_1() throws InvalidInputException {
+        int foundIndex = subString.kmpSubstringSearch("bacbababaabcbab", "ababaca");
+
+        assertEquals(-1, foundIndex);
+    }
+
+    @Test
+    public void canDoKMPSubStringSearch_2() throws InvalidInputException {
+        int foundIndex = subString.kmpSubstringSearch("bacbababadabababaacbab", "ababaa");
+
+        assertEquals(12, foundIndex);
+    }
+
+    @Test(expected = InvalidInputException.class)
+    public void cannotDoKMPSubstringSearchWithInvalidText() throws InvalidInputException {
+        subString.kmpSubstringSearch("a", null);
+    }
+
+    @Test(expected = InvalidInputException.class)
+    public void cannotDoKMPSubstringSearchWithInvalidPattern() throws InvalidInputException {
+        subString.kmpSubstringSearch(null, "a");
+    }
+
 
 }
